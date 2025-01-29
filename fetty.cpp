1f5b1a8b97496078ca275634e78088c0f26c6d04
neelyp <neel.pandruvada@mcvts.org>
2025-01-29 10:43:38 -0500

    worked
    Co-authored-by: AyaanG123 <AyaanG123@users.noreply.github.com>
    Co-authored-by: atharvaru <atharvaru@users.noreply.github.com>

--- fetty.cpp
+++ fetty.cpp
@@ -17,41 +17,76 @@ int main()
     int choice;
     string addition;
     string additionextra;
+    char keepGoing = 'y';
 
-    cout << "What u finna do megatron";
-    cin >> choice;
-
-    switch (choice)
+    while (keepGoing == 'y')
     {
-    case 1:
-        // select song
-        // print vector
-        // give choice
-        // play song
+        cout << "What u finna do megatron";
+        cin >> choice;
 
-        break;
-    case 2:
-        // add song
-        cout << "Enter Song Addition: ";
-        getline(cin, addition);
-        cout << "Enter Artist: ";
-        getline(cin, additionextra);
-        cout << endl;
-        playlist.push_back({addition, additionextra});
-    case 3:
-        // play in order
-        printVector(playlist);
-    case 4:
-        // play in shuffle
-        break;
-    default:
-        // code block
+        switch (choice)
+        {
+        case 1:
+            // select song
+            int song;
+            printVector(playlist);
+            cout << "Enter Song Number to play: ";
+            cin >> song;
+
+            cout << "Currently Playing: " << playlist[song - 1][0] << " by " << playlist[song - 1][1] << endl;
+            // code to play song
+            break;
+        case 2:
+            // add song
+            cout << "Enter Song Addition: ";
+            getline(cin, addition);
+            cout << "Enter Artist: ";
+            getline(cin, additionextra);
+            cout << endl;
+            playlist.push_back({addition, additionextra});
+
+            break;
+        case 3:
+            // play in order
+            for (const auto &song : playlist)
+            { // shortcut to loop through vectors
+                cout << "Currently Playing: " << song[0] << " by " << song[1] << endl;
+                // code to play audio file of the song
+
+                time_t start_time = time(nullptr);
+                while (difftime(time(nullptr), start_time) < 30)
+                {
+                }
+            }
+            break;
+        case 4:
+            // play in shuffle
+            for (const auto &song : playlist)
+            {
+                srand(time(0));
+                int ranum = 0 + (rand() % 5);
+
+                cout << "Currently Playing: " << song[ranum] << " by " << song[ranum + 1] << endl;
+                // code to play audio file of the song
+
+                time_t start_time = time(nullptr);
+                while (difftime(time(nullptr), start_time) < 30)
+                {
+                }
+            }
+            break;
+        default:
+            cout << "That is not an option, try again";
+            break;
+        }
+        cout << "Would you like to keep going? ";
+        cin >> keepGoing;
     }
 
     return 0;
 }
 
-void printVector(vector<vector<string>> playlist);
+void printVector(vector<vector<string>> playlist)
 {
     for (int i = 0; i < playlist.size(); i++)
     {
